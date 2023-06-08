bb417f02c0d7d03cc91f0efb24752906b5197799
JOSE RICARDO RUGERIO SAN VICENTE <jrugeriosanvicente@tenaris.com>
2023-06-08 15:55:13 -0600

    creating user controls namespace usage

---
+++ CafeApp/View/CustomersView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CafeApp.View
+{
+    /// <summary>
+    /// Interaction logic for CustomersView.xaml
+    /// </summary>
+    public partial class CustomersView : UserControl
+    {
+        public CustomersView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMoveClick(object sender, RoutedEventArgs e)
+        {
+            //var valueColumnProperty = (int) this.gridCustomerList.GetValue(Grid.ColumnProperty);
+
+            //var newvalueColumnProperty = valueColumnProperty == 0 ? 2 : 0;
+            //this.gridCustomerList.SetValue(Grid.ColumnProperty, newvalueColumnProperty);
+
+            var column = Grid.GetColumn(this.gridCustomerList);
+
+            var newColumn = column == 0 ? 2 : 0;
+            Grid.SetColumn(this.gridCustomerList, newColumn);
+        }
+    }
+}
